ac44f748f7d4b03dd3fcb1e30dc13d4abf89f9f4
Idalia Mercado <immp1986@yahoo.es>
2025-02-17 14:41:14 -0500

    SpringBoot project

---
+++ Angular-SpringBootProject/SystemSolar/Stringboot-helloworld-application/src/test/java/net/javaguides/springboot/Stringboot_helloworld_application/StringbootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.Stringboot_helloworld_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringbootHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
